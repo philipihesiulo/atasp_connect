3fbd643b9977aded401363468412048c51ad7428
Nwasiulo <philipihesiulo@gmail.com>
2023-06-09 07:49:05 +0000

    Added Wew Web Forms

--- atasp_connect/atasp_connect/doctype/generic_beneficiary_profile/test_generic_beneficiary_profile.py
+++ atasp_connect/atasp_connect/doctype/generic_beneficiary_profile/test_generic_beneficiary_profile.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 # import frappe
 import unittest
 
-class TestBeneficiaryProfile(unittest.TestCase):
+class TestGenericBeneficiaryProfile(unittest.TestCase):
 	pass
