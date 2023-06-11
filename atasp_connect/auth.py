2d9637eb201a9515ba0b1e5372fa618cf7c41d9b
Nwasiulo <philipihesiulo@gmail.com>
2023-06-11 21:52:57 +0000

    refactored authentication response for API

--- atasp_connect/auth.py
+++ atasp_connect/auth.py
@@ -12,7 +12,7 @@ def validate():
             frappe.response["zone"] = atasp_connect_user.zone
             role_profile = user_account.role_profile_name
         
-            if role_profile == "":
+            if role_profile == "" or role_profile == None:
                 error_message = _("A role has not been set for your account")
                 frappe.throw(error_message)
             else:
