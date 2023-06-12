38090d7b53b7942ca39d146104927426d4bf68d9
Nwasiulo <philipihesiulo@gmail.com>
2023-06-12 21:07:00 +0000

    created custom user management functionality for ATASP Connect

--- atasp_connect/monitoring_&_evaluation/doctype/quarterly_performance_data/quarterly_performance_data.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2021, Philip Ihesiulo and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class QuarterlyPerformanceData(Document):
-	pass
