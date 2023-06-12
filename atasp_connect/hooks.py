52a6a00b436e2f29475de91928a808136d5c0c6d
Nwasiulo <philipihesiulo@gmail.com>
2023-06-12 21:32:07 +0000

    created custom user management functionality for ATASP Connect

--- atasp_connect/hooks.py
+++ atasp_connect/hooks.py
@@ -83,7 +83,7 @@ home_page = "login"
 permission_query_conditions = {
 	"Quarterly Performance": "atasp_connect.permissions.quarterly_performance_query",
 	"Zone": "atasp_connect.permissions.zone_query",
-	"Role": "atasp_connect.permissions.role_query",
+	#"Role": "atasp_connect.permissions.role_query",
 }
 #
 # has_permission = {
