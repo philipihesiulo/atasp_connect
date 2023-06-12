bbfcd9a5ec211fb3ccf68ace2ede806005628d30
Nwasiulo <philipihesiulo@gmail.com>
2023-06-12 00:01:56 +0000

    refactored authentication response for API

--- atasp_connect/auth.py
+++ atasp_connect/auth.py
@@ -4,9 +4,12 @@ from frappe import _
 def validate():
     logged_in_user = frappe.session.user
     error_message = ""
-    if(not (logged_in_user == "Guest" or logged_in_user == "Administrator")):
+    if not (logged_in_user == "Guest" or logged_in_user == "Administrator"):
         try:
             user_account = frappe.get_doc('User', logged_in_user)
+            if not frappe.db.exists('ATASP Connect User', logged_in_user):
+                error_message = _("Your ATASP Connect Account is not active")
+                frappe.throw(error_message)
             atasp_connect_user = frappe.get_doc('ATASP Connect User', logged_in_user)
             frappe.response["email"] = user_account.email
             frappe.response["zone"] = atasp_connect_user.zone
@@ -17,11 +20,11 @@ def validate():
                 frappe.throw(error_message)
             else:
                 frappe.response["role"] = role_profile
+                frappe.response["status"] = "success"
                 
         except Exception as e:
-            error_message = error_message if error_message != "" else _("Your ATASP Connect Account is not active")
             frappe.local.login_manager.logout()
             frappe.response.clear()
+            frappe.response["status"] = "error"
             frappe.response["message"] = error_message
-            #frappe.throw(error_message)
             
